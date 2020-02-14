fd769d34c41dc52f25ffdd4ac08183096921bad9
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-02-14 11:42:28 +0200

    5-6 diena

---
+++ dirbame_cia/diena_5/destytojas/1-js-pagrinddai/master.js
@@ -0,0 +1,17 @@
+console.log(" as veikiu");
+
+// komentaras
+
+/*
+ ilgas
+ komentraras
+ */
+ // senovinis kintamasis
+ // kintamuju tipai
+var  x = 10;   // number (integer - sveikasis)
+var  y = 9.99; // number ( float/ double - su kableliu)
+var vardas = "tautvydas";  // string (tekstas)
+var arVedes = true;   // boolean true,  false (tiesa , netiesa)
+// uzimti pavadinimai:  true, false, var, let, function, return,
+var mirtiesData; // undifined - sukurtas kintamasis, bet be duomenu
+var arYraKlaidu = null;   // 
