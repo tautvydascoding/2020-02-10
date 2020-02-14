5f4d0f62e757740960ab4210dcc2f71e02c93941
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-02-14 16:25:31 +0200

    klasses darbas

--- dirbame_cia/diena_5/destytojas/1-js-pagrinddai/master.js
+++ dirbame_cia/diena_5/destytojas/1-js-pagrinddai/master.js
@@ -14,4 +14,37 @@ var vardas = "tautvydas";  // string (tekstas)
 var arVedes = true;   // boolean true,  false (tiesa , netiesa)
 // uzimti pavadinimai:  true, false, var, let, function, return,
 var mirtiesData; // undifined - sukurtas kintamasis, bet be duomenu
-var arYraKlaidu = null;   // 
+var arYraKlaidu = null;   //
+
+vardas = "Lukas";
+console.log(  vardas );
+vardas = "Rokas";
+console.log(  vardas );
+console.log(  vardas );
+console.log(  vardas );
+
+//  pavadinimas (variable)  = reiksme (value)
+let pavarde = "Gete";
+// KLAIDA - let neleidzia sukurti du kart vienodo kintamojo
+// let pavarde = "Jonaitis";
+
+let atsakymas = (10 * 5) + 2;
+console.log(  "atsakymas: " ,  atsakymas  );
+atsakymas = ((((2 + 3))));
+
+atsakymas = Math.sqrt( 9 ); // saknies traukimas 3
+console.log( "saknis is 9: ", atsakymas);
+
+atsakymas = Math.PI + 1 ; // 3.1415.... + 1
+console.log( "pi: ", atsakymas);
+
+atsakymas = Math.round( 9.99 ) ; // 9.99 -> 10
+console.log( "apvalintas skaicius: ", atsakymas);
+
+let pirmas = 100;
+let antras = 30;
+atsakymas = pirmas - antras;
+console.log( "atimtis: ", atsakymas);
+console.log( "====pratimas vienas=====");
+
+//
