1d5607b443e1a9cd5866a6dea174e61275530091
vytautaskrapikas@gmail.com <vytautaskrapikas@gmail.com>
2020-02-15 14:33:21 +0200

    5 diena js ir bootstrap

---
+++ dirbame_cia/diena_5/Vytautas/1-js-pagrindai/2-js-atlyginimo-skaiciavimas/master.js
@@ -0,0 +1,9 @@
+let alga = 890;
+let PSD = 9; //procentai
+let VSD = 24; //%
+let atsakymas;
+let mokesciai;
+
+
+atsakymas = alga - (alga * PSD / 100) - ((alga/2) * VSD / 100)
+console.log("alga po mokesciu:", atsakymas );
---
+++ dirbame_cia/diena_5/Vytautas/1-js-pagrindai/2-js-atlyginimo-skaiciavimas/uzduotys-0-js-kintamieji.js
@@ -0,0 +1,9 @@
+
+// UZDUOTIS
+// sukaiciuoti kokia jusu atlyginima i rankas, jeigu:
+// atlyginimas ant popieriaus 890
+// o mokesciai:
+// PSD 9%
+// VSD 24% nuo puses atlyginimo
+// sukikurti 4-5 kintamuosius
+// ir apskaiciuoti atlyginima i rankas
