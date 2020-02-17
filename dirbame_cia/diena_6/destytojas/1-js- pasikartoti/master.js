580941920369209b4272384e6cd00cdb31f82d1f
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-02-17 16:35:48 +0200

    klsases darbas

--- dirbame_cia/diena_6/destytojas/1-js- pasikartoti/master.js
+++ dirbame_cia/diena_6/destytojas/1-js- pasikartoti/master.js
@@ -97,8 +97,9 @@ function atimtis(x, y, z, f, g) {
     let ats = x - y - z - f - g;
     console.log( "atimtis   : " + ats);
 }
-atimtis(100, 1, 1, 1, 1);
-atimtis(-2, -2, -2, -2, -2);
+
+ atimtis(1, 1 ,1,1,1    );   // Petro atimtis
+atimtis(-2, -2, -2, -2, -2); // Martynas atimtis
 
 // 2.2 UZDUOTIS
 // parasyti f-ja "pazymiuVidurkis1(x1, x2, x3, x4, x5)", kuriai galima paduoti 5 kintamuosius,
@@ -107,16 +108,45 @@ atimtis(-2, -2, -2, -2, -2);
 
 
 //=====================RETURN==================
+function skaiciuotiPajamas(x1, x2) {
+    let ats = x1 + x2;
+    return ats;
+}
+//console.log(   ats  );  // ERROR - not defined variable
+let pajamos = skaiciuotiPajamas(1000, 1500);
+console.log(   pajamos * 0.21 ); // skaicuojam mokesciu 21pvm
 
 // 1 UZDUOTIS
 // A) parasyti f-ja "getVardas()", kuri turi "return" zodeli ir grazina varda i iskveitimo vieta.
 // B)   f-joje  sukurti kintamaji "vardas" Tomas
 // C) patikrinti ar veikia f-ja
+function getVardas() {
+    let vardas = "Tomas";
+    return vardas;
+}
+console.log(    getVardas()  );
+console.log(    getVardas() + " Tomaitis" );
+console.log(    getVardas()  );
+
 
 // 2 UZDUOTIS
 // parasyti f-ja "getVardasPavarde()", kuri turi "return" zodeli ir grazina varda ir pavarde i iskveitimo vieta.
 //  f-joje sukurti kintamaji   "pavarde" Tomauskas, "vardas" - Antanas
 // patikrinti ar veikia f-ja
+
+// f-jos sukurimas
+function getVardasPavarde() {
+    let vardas = "tomas";
+    let pavarde = "tomaitis";
+    let tekstas = vardas + " " + pavarde; // string sulipdome is keliu gabalu
+    return tekstas;
+}
+// alert(   getVardasPavarde() );
+
+// f-jos paleidimas
+console.log(    getVardasPavarde()  );
+// arba
+document.querySelector('body').innerHTML += getVardasPavarde() + "<br>";
 
 // 3 UZDUOTIS
 // A) parasyti f-ja "getPelnas()", kuri turi "return" zodeli ir grazina apskaiciuota pelna
@@ -124,17 +154,40 @@ atimtis(-2, -2, -2, -2, -2);
 // B) f-joje sukurti kintamaji "islaidos" 7500
 // B) f-joje apskaiciuoti pelna  ( pvz: pelnas = pajamos - islaidos)
 // C) patikrinti ar veikia f-ja
+function getPelnas() {
+    let pajamos = 13500;
+    let islaidos = 7500;
+    let pelnas = pajamos - islaidos;
+    return pelnas;
+}
+console.log( "sausio pelnas: " + getPelnas() );
+console.log( "vasario pelnas: " + getPelnas() );
+console.log( "kovas pelnas: " + getPelnas() );
 
 // 3.2 UZDUOTIS
 // A) parasyti f-ja "getPelnas22(pajamos, nuostoliai)", kuri turi "return" zodeli ir grazina apskaiciuota pelna
 // B) f-joje apskaiciuoti pelna  ( pvz: pelnas = pajamos - nuostoliai)
 // C) patikrinti ar veikia f-ja
-
+function getPelnas22( pajamos, islaidos) {
+    let pelnas = pajamos - islaidos;
+    return pelnas;
+}
+console.log( "sausio pelnas: "  + getPelnas22(6000, 4000) );
+console.log( "vasario pelnas: " + getPelnas22(10000, 6000) );
+console.log( "kovas pelnas: "   + getPelnas22(5000, 6000) );
 
 // 4.1 UZDUOTIS
 // parasyti f-ja, kuri skaiciuoja vieno mokinio pazymiu vidurki
 // "getPazymiuVidurkis2(x1, x2, x3, x4, x5)"  f-ja apskaiciuoja vidurki is paduotu 5 menesiu pazymiu (ir grazina atsakyma i iskvietimo vieta! "return atskymas;")
 //  turi "return" zodeli.
+function getPazymiuVidurkis2(x1, x2, x3, x4, x5) {
+    let ats = (x1 + x2 + x3 + x4 + x5) / 5;
+    return ats;
+}
+console.log("Ieva: "    + getPazymiuVidurkis2(4,4,5,7,4)  );
+console.log("Antanas: " + getPazymiuVidurkis2(8,8,8,8,8)  );
+console.log("TOmas: "   + getPazymiuVidurkis2(4,4,0,0,4)  );
+console.log("Aris: "    + getPazymiuVidurkis2(10,10,10,10,5)  );
 
 // 5 UZDUOTIS
 // A) sukurti h1 su tekstu viduje  (HTML faile)
--- uzduotys/diena_6/UZDUOTYS/uzduotys-2-f-jos-return-verslas.js
+++ uzduotys/diena_6/UZDUOTYS/uzduotys-2-f-jos-return-verslas.js
@@ -7,7 +7,7 @@ valandos Tomas   = 100
 imones pajamos = 20000
 kitos imones islaidos = 3400
  UZDUOTIS:
- 1) sukurti  f-ja: getDarbuotojoAtlyginimas
+ 1) sukurti  f-ja: getDarbuotojoAtlyginimas(valandos, valIkainis)
  1.2) paleisti tiek kartu kiek turime darbuotoju
  2) sukurti  f-ja: getImonesPelnas
  2.2) isvesti apskaiciuota pelna i console ar ekrana
