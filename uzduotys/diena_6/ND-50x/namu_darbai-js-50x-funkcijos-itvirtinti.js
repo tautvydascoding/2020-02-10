8c1aac97f570581a65cb876cdcd68315d4656dfd
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-02-17 17:02:33 +0200

    klases darbas

--- uzduotys/diena_6/ND-50x/namu_darbai-js-50x-funkcijos-itvirtinti.js
+++ uzduotys/diena_6/ND-50x/namu_darbai-js-50x-funkcijos-itvirtinti.js
@@ -6,7 +6,7 @@
 // setVardas2("Juozas");
 // getVardas2();
 // A) sukurti (gloabalu) kintamaji pvz:
-//      var vardas =  "Tomas";
+     // var vardas =  "Tomas";
 // B) sukurti 20-30 f-ju:  getVardas1(), getVardas2(), getVardas3(), ...getVardas30()
 // kurios  turi "return" ir iskvietus f-ja ji  grazina varda ir prideti skaiciu salia  pvz: "Tomas1", Antanas22, Juozas33
 // B) sukurti 20-30 f-ju:   setVardas1(name), setVardas2(name), ... setVardas30(name),...
@@ -35,7 +35,7 @@ setVardas2("antanas"); // sugalvoti vis kita zodi
 function getVardas2() {
     return vardas + "2";
 }
-x =  getVardas2();     
+x =  getVardas2();
 console.log(  x );  // isveda: antanas22
 
 // 3---------------------
