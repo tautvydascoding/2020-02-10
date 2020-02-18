52e464e7cba610fd9e4bad9becf9d880150b12ac
karoliszagarnauskas <35958904+karoliszagarnauskas@users.noreply.github.com>
2020-02-18 17:07:56 +0200

    Revert "Merge branch 'master' of https://github.com/tautvydascoding/2020-02-10"
    This reverts commit a30a297c3103b13b2ffc3fa80e86984bf13419c1, reversing
    changes made to b7691a186e9d26995f9ed19c8cb9ed7d0c8f7751.

--- dirbame_cia/diena_6/Aiste/java script/5 h1 pakeitimas/master.js
+++
@@ -1,6 +0,0 @@
-
-// 5 UZDUOTIS
-// A) sukurti h1 su tekstu viduje  (HTML faile)
-// B) sukurti f-ja "getH1ElementoAukstis(), kuri su "return" grazina surenderinto h1 elemento auksti
-// C) su CSS pakeisti h1 auksti, ir paziureti ar jusu f-ja veikia
-
