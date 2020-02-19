ad67f38949ec61a2151674cec0ee2b6836b1e87a
vitvait@yahoo.com <48964852+Vitos2@users.noreply.github.com>
2020-02-19 13:20:53 +0200

    6d formos su if

---
+++ dirbame_cia/diena_6/vitos/4 JS uzd apskritimas/js/main2.js
@@ -0,0 +1,13 @@
+console.log("veikia");
+
+function apskritimoPlotas (){
+
+    let ivedimoLaukas = document.querySelector('#apskritimo');
+    // console.log(ivedimoLaukas); //testin
+
+    let ivestasSkaicius = ivedimoLaukas.value;
+    // console.log("ivedimas:", ivestasSkaicius);
+
+
+
+}
