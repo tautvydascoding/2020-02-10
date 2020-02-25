98e9b81c9ab187171e7780647b94b941dafc08df
Dalia <budriened@gmail.com>
2020-02-25 12:37:52 +0200

    11 diena

---
+++ dirbame_cia/diena_11/Dalia/php-include-prekes-4/template-preke.php
@@ -0,0 +1,7 @@
+
+<article class="col-3 bg-warning m-1 p-2">
+    <h2><?php echo $antraste ?></h2>
+    <p><?php echo $aprasymas ?></p>
+
+    <button type="button" name="button"><?php echo $kaina ?></button>
+</article>
