5b5be1bad77bee189c9a9f1dcd81f6ccf0c9a57e
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-02-27 14:39:22 +0200

    klase sdarbas create doctor

--- dirbame_cia/diena_14/destytojas/1-PHP-CRUD-library/models/doctors.php
+++ dirbame_cia/diena_14/destytojas/1-PHP-CRUD-library/models/doctors.php
@@ -15,3 +15,33 @@ if ($prisijungimas) {
 } else {
     echo "ERROR: nepavyko prisijungti prie DB:" . mysqli_connect_error($prisijungimas);
 }
+function getPrisijungimas() {
+    // isvardini globalius kint. kuriuos nori naudoti f-jos viduje
+    global $prisijungimas; // !! sioje eilute, globaliu kint. negalima keisti, bet zemiau galima
+    return $prisijungimas;
+}
+
+function deleteDoctor($nr) {
+    $manoSQL = "DELETE FROM doctors WHERE id = '$nr'  LIMIT 1";
+    $arPavyko = mysqli_query( getPrisijungimas(),  $manoSQL  );
+    if ( $arPavyko == false) {   // !$arPavyko
+        echo "ERROR nepavyko atleisti gydytojo nr: $nr <br>";
+    }
+}
+// deleteDoctor(6); // test
+/*
+    i DB irasysim nauja gydytoja
+    $vardas - gydytojo vardas
+    $pavarde - gyd. pavarde
+    $zona - gyd. zona kurios pacientus aptarnauja
+*/
+function createDoctor($vardas, $pavarde, $zona) {
+    $manoSQL = "INSERT INTO  doctors VALUES( NULL, '$vardas', '$pavarde', '$zona' )";
+    $arPavyko = mysqli_query( getPrisijungimas(),  $manoSQL  );
+    if ( $arPavyko == false) {   // !$arPavyko
+        echo "ERROR nepavyko sukurti gydytojo vardas: $vardas, $pavarde, $zona <br>";
+    }
+}
+// test
+// createDoctor('Jurgis', 'Jurgaitis', 'A3');
+// createDoctor('Tadas', 'Tadauskas',  'B2');
