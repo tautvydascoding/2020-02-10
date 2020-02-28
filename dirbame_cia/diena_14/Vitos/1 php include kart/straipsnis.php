62635d0935a57af166b74322b0457a474c878c86
vitvait@yahoo.com <48964852+Vitos2@users.noreply.github.com>
2020-02-28 21:03:26 +0200

    14d - php include ir DB

---
+++ dirbame_cia/diena_14/Vitos/1 php include kart/straipsnis.php
@@ -0,0 +1,11 @@
+
+<div class="">
+<main>
+    <article class="">
+        <?php echo "$vardas"."$pavarde"; ?>
+       <h2> <?php echo "$straipsnis[0]"; ?> </h2>
+       <p> <?php echo "$straipsnis[1]"; ?>  </p>
+       <div>Data:   <?php echo "$straipsnis[2]"; ?>    </div>
+    </article>
+</main>
+</div>
