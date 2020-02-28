e29cb56d7186972d35575a461545dcd02d569f93
Dalia <budriened@gmail.com>
2020-02-28 09:10:24 +0200

    13 diena

---
+++ dirbame_cia/diena_13/Dalia/jQuery-onclick-event-4/master.js
@@ -0,0 +1,16 @@
+console.log(" as veikiu");
+
+
+
+// $('h1').css();
+// $('h1').txt()
+// $('h1').css('display','none');
+// $('h1').css('color', 'grey ');
+// $('h1').show(3000);
+$('footer').click(function(event){
+
+    $('h1').animate({
+        "font-size" : "5px";
+        "opacity" : 0.1
+    }, 3000);
+});
--- dirbame_cia/diena_13/Dalia/php-matrix-visos-prekes-3/index.php
+++ dirbame_cia/diena_13/Dalia/php-matrix-visos-prekes-3/index.php
@@ -6,9 +6,9 @@ sukurti matrica, kurioje bus saugoma kiekvienos prekes info:
 Antraste, img pavadinimas, kaina, prekes aprasymas -->
 <?php
 
-$preke1 = ['Batai', 'nuotrauka', 'Moteriski batai', '100 eur'];
-$preke2 = ['Kelnes', 'nuotrauka', 'Dzinsines kelnes', '80 eur'];
-$preke3 = ['Kepure', 'nuotrauka', 'Kepure nuo saules', '20 eur'];
+$preke1 = ['Batai', '1.jpg', 'Moteriski batai', '100 eur'];
+$preke2 = ['Kelnes', '1.png', 'Dzinsines kelnes', '80 eur'];
+$preke3 = ['Kepure', '2.png', 'Kepure nuo saules', '20 eur'];
 $visosPrekes = [$preke1, $preke2, $preke3];
 
 
@@ -16,24 +16,26 @@ $visosPrekes = [$preke1, $preke2, $preke3];
 // B. i masyva visosPrekes, ideti "preke" masyva
 // C. atvaizduodi kiekvienoje eiluteje po 3 prekes (naudojant for arba foreach)
 for ($i=0; $i < count($visosPrekes) ; $i++) {
-    echo "<article clas ='col-4'>";
-    echo "<h2>" . $visosPrekes [$i] [0] . "</h2>";
-    echo "img src='img/"$visosPrekes [$i] [1] . "'>";
-    echo "<p>" . $visosPrekes [$i] [2] . "</p>" ;
+    echo "<article  width='30% style='float:left' >";
+    echo "<h2>  {$visosPrekes [$i] [0]} </h2>";
+    echo "<img src='img/" . $visosPrekes [$i] [1] . "' width =  '10%' >";
+    echo "<p>  {$visosPrekes [$i] [2]}  </p>" ;
     echo "<a class = 'btn btn-lg  bg-info'>" . $visosPrekes [$i] [3] . "</a>";
     echo "</article>";
 }
+echo "<div style='clear:both;'   > </div>";
 
-// ir bootstrap dizaina
-// <article clas='col-4'>
-//     <h2> ...
-//     <img>
-//     <p>...
-//     <button> ...
-//     <a class="btn btn-lg  bg-info"> </a>
-// </article>
+for ($i=0; $i < count($visosPrekes) ; $i++) {
+    ?>
+<article width ="30%" style="float:left">
+    <h2>
+        <?php echo $visosPrekes [$i][0] ?>
+    </h2>
+
+</article>
 
 
+<?php  }
    // 3 budai nuotraukoms
     // printf("<img src='./img/" . $visosPrekes[$i][1] . "' width='200px;' alt=''  />");
     // printf("<img src='./img/%s' width='200px;' alt=''  />", $visosPrekes[$i][1]);
