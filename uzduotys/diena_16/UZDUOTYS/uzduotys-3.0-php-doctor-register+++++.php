6c16f35fcf855a62d78666f7a77b6d11c62f94a2
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-03-02 10:36:12 +0200

    pataisyta uzduotis

--- uzduotys/diena_16/UZDUOTYS/uzduotys-3.0-php-doctor-register+++++.php
+++ uzduotys/diena_16/UZDUOTYS/uzduotys-3.0-php-doctor-register+++++.php
@@ -1,11 +1,12 @@
-<!--
- HEAD:uzduotys/diena_16/UZDUOTYS/uzduotys-3.0-php-doctor-register.php
+ 
+
 // // createDoctor() paleidimas per narsykle
 //
 // // 1.1 sukurti PHP faila  naujasGydytojas.php
 // // 1.2. faile paleisti:
-// include('db_functions.php');
-// createDoctor("Juozas", "Juozaitis");
+// include('models/doctors.php');
+// createDoctor("Juozas", "Juozaitis", "Z1");
+// echo "pavyko sukurti";
 // // 1.3. Narsykleje nueiti:
 //     localhost/ ... /naujasGydytojas.php
 //
@@ -13,27 +14,10 @@
 // deleteDoctor() paleidimas per narsykle
 // // 2.1. sukurti PHP faila  trintiGydytoja.php
 // // 2.2. faile paleisti:
-// include('db_functions.php');
+// include('models/doctors.php');
 // deleteDoctor(4);
+// echo "istrynet sekmingai";
 // // 2.3. Narsykleje nueiti:
 //     localhost/ ... /trintiGydytoja.php
 
 // createDoctor() paleidimas per narsykle
-
-// 1.1 sukurti PHP faila  naujasGydytojas.php
-// 1.2. faile paleisti:
-include('db_functions.php');
-createDoctor("Juozas", "Juozaitis");
-echo "pacyko sukurti nauja gydytoja<br>";
-// 1.3. Narsykleje nueiti:
-    localhost/ ... /naujasGydytojas.php -->
-
-
-
-<!-- // 2.1. sukurti PHP faila  trintiGydytoja.php
-// 2.2. faile paleisti:
-include('db_functions.php');
-deleteDoctor(4);
-echo "pavyko istrinti  gydytoja <br>";
-// 2.3. Narsykleje nueiti:
-    localhost/ ... /trintiGydytoja.php
