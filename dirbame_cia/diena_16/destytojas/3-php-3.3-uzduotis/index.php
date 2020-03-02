9860018fc3892acce17c7997eef001ca5f8a7b26
tautvydascoding@gmail.com <tautvydascoding@gmail.com>
2020-03-02 14:51:46 +0200

    klases darbas

---
+++ dirbame_cia/diena_16/destytojas/3-php-3.3-uzduotis/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Gydytojo registracija</h1>
+        <a href="form-doctor-register.php">    + New Doctor </a>
+
+
+
+    </body>
+</html>
