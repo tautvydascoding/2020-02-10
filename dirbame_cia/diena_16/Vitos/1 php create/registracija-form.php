2195fd6e7532bf1e904d1cd72e9fc4acce8927bf
vitvait@yahoo.com <48964852+Vitos2@users.noreply.github.com>
2020-03-03 20:05:20 +0200

    16d -php register

---
+++ dirbame_cia/diena_16/Vitos/1 php create/registracija-form.php
@@ -0,0 +1,7 @@
+
+<form class="" action="naujasGydytojas.php" method="get">
+    <label for="vardas"> Jusu vardas: </label> // lipdukas salia
+    <input type="text" name="vardas" value="">
+    <input type="text" name="pavarde" value="">
+<button type="submit" name="button">Kurti</button>
+</form>
