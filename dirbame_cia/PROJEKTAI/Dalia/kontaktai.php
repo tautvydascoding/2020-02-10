a56fe6afc9a0362e0ea0bf3763b2bb3d5b1995fe
Dalia <budriened@gmail.com>
2020-03-11 08:55:07 +0200

    Projektas

--- dirbame_cia/PROJEKTAI/Dalia/kontaktai.php
+++ dirbame_cia/PROJEKTAI/Dalia/kontaktai.php
@@ -1,18 +1,29 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-    </head>
-    <body>
-        <h1>kontaktu puslapis</h1>
-        <h2>Klausikite</h2>
-        <form action="email.php" method="POST">
-  <input type="text" id="vardas" placeholder="Vardas"name="vardas"><br>
-  <input type="email" name ="pastas" placeholder="El.pastas" required><br>
-  <textarea name="klausimas"placeholder="klausimas" rows="8" cols="80"></textarea><br>
-  <button type="submit" value="Submit">Klauskite<br>
+<?php
+
+include "header.php";
+?>
+<section class="container bg-light">
+    <div class="row">
+        <div class="col-3">
+
+            <h1 class="text-success" >Susisiekime</h1>
+            <br>
+            <form action="email.php" method="POST">
+                <input type="text" id="vardas" placeholder="Jūsų vardas"name="vardas" class="form-control"><br>
+                <input type="email" name ="pastas" placeholder="Jūsų el. paštas" class="form-control" required><br>
+        </div>
+<section class="container">
+    <div class="row">
+  <textarea name="klausimas"placeholder="Žinutė" rows="8" cols="80" class="form-control col-7" ></textarea><br>
+    </div>
+<br>
+  <div class="row">
+  <button type="submit" value="Submit" class="btn btn-secondary col-1">Siųsti<br>
+      </div>
 </form>
 
-    </body>
-</html>
+</section>
+
+</section>
+</header>
+<?php include "footer.php"; ?>
