fc9bd0a5596158908645da2bbae1b54acff28737
mageci <samdo1987@gmail.com>
2020-03-16 10:07:52 +0200

    bootstrap

--- dirbame_cia/-tuscias--projektas-3/index.php
+++ dirbame_cia/-tuscias--projektas-3/index.php
@@ -17,13 +17,9 @@
 
     <body>
 
-        <div class="container">
-            <header class="row">
-                <div class="col-md-12">
-                    <h1> Isijunk Inspect Element->console </h1>
-                </div>
-            </header>
 
+
+</div>
 
 
             <footer class="row">
---
+++ dirbame_cia/diena_20/mantasgec/bootstrap-kontrolinis/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+        <!-- reikalinga kad prisitaikantis dizainas veiktu tvarkingai -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+        <!-- galimos klaidos -->
+        <!-- blogas kelias iki failo -->
+        <!-- "/" ne i ta puse -->
+        <!-- neuzdarete ">" -->
+        <link rel="stylesheet" href="css/master.css">
+        <!-- !!!! VISSA MANO CSS failas pats zemiausias -->
+    </head>
+
+    <body>
+        <div class="container ">
+            <header class="row">
+                <div class="col-sm-8 bg-secondary aukstis-100">
+                </div>
+            </header>
+            <div class="row">
+                <div class="col-sm-4 mt-2 mr-2 aukstis-100 bg-secondary">
+                    <div class="row">
+                        <div class="col-sm-12 bg-secondary aukstis-100">
+                        </div>
+                    </div>
+                    <div class="row mt-2">
+                        <div class="col-sm-4 mr-2 aukstis-100 bg-secondary">
+                        </div>
+                        <div class="col-sm aukstis-100 bg-secondary">
+                        </div>
+                    </div>
+                    <div class="row-sm-4 mt-2">
+                        <div class="col-sm-8 bg-secondary aukstis-300">
+                        </div>
+                    </div>
+                    <div class="row-sm-4 mt-2">
+                        <div class="col-sm-8 bg-secondary aukstis-200">
+                        </div>
+                    </div>
+                    <div class="row-sm-4 mt-2 ">
+                        <div class="col-sm-8 bg-secondary aukstis-100">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mt-2">
+                    <div class="row-sm-4 bg-secondary aukstis-100">
+                    </div>
+                    <div class="row-sm-4 mt-2 bg-secondary aukstis-100">
+                    </div>
+                    <div class="row-sm-4 mt-2 bg-seco aukstis-100">
+                    </div>
+                </div>
+
+            </div>
+            <footer class="row">
+                <div class="col-md-12 ">
+                    <div class="row mt-2 bg-secondary aukstis-10">
+                    </div>
+                </div>
+            </footer>
+        </div>
+
+
+
+
+        </div>
+        <!-- Scriptai FAILO dugne -->
+        <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script> -->
+        <script type="text/javascript" src="libs/jQuery/jquery-3.3.1.min.js" ></script>
+        <script type="text/javascript" src="libs/bootstrap/js/bootstrap.bundle.min.js">    </script>
+        <!--  mano js pats zemiausias!!!-->
+        <script type="text/javascript" src="master.js"></script>
+
+    </body>
+</html>
